bef9a839a5eda2fe77154e67cccd922783d4f0c7
shivam <shivam7214376@gmail.com>
2024-05-06 16:44:53 +0530

    function

---
+++ function.py
@@ -0,0 +1,8 @@
+def Calculategmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+a=int(input())
+b=int(input())
+ans=Calculategmean(a,b)
+print(ans)
---
+++ loops.py
@@ -0,0 +1,17 @@
+# # for loop
+# name="Shivam dubey"
+# colors=["Red","Blue","Green","Yellow"]
+# for i in name:
+#     print(i)
+# for i in colors:
+#       print(i)
+
+# for k in range(6):
+#      print(k+1)
+
+
+#while loop
+i=0
+while(i<3):
+    print(i)
+    i=i+1
