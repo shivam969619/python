a446a77c42ae559b65a0bd662b038a695654626f
shivam <shivam7214376@gmail.com>
2024-05-06 15:29:31 +0530

    strings

---
+++ main3.py
@@ -0,0 +1,9 @@
+a="shivam!!!!!!"
+# string are immutable belows is a new string
+print(a.upper()) 
+print(a.lower()) 
+print(a.rstrip("!"))
+print(a.replace("shivam","dubey"))
+print(a.split("!"))
+print(a.capitalize())
+print(a.count("s"))
