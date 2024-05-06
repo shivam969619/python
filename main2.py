3c20cd1247e4204a89d5878f85e53cefba7b7042
shivam <shivam7214376@gmail.com>
2024-05-06 10:34:37 +0530

    strings

---
+++ main2.py
@@ -0,0 +1,15 @@
+# slicing in python
+names="shivam,dubey"
+print(names[0:6])
+# 0 se 6 char tak print krega agar 6 kejagah minus me kuch ho usko python length me jodne ke baad jo aaee vaha tak lega
+# for length
+print(len(names)) 
+print (names[0:-3])
+m="lund"
+print(m)
+print(m[0])
+for i in m:print(i)
+
+nm="Harry"
+print(nm[-4:-2])
+
